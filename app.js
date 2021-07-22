9dc21a304708f46bdabb13f7bbf85aa54aae8bb3
Sattvik Palta <sattvikpalta@gmail.com>
2021-07-22 10:51:50 +0530

    Created ball, paddle and scoreboard

--- app.js
+++ app.js
@@ -1,6 +1,66 @@
+// Canvas API - https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+let score = 0;
+
+// Create ball props
+const ball = {
+  x: canvas.width / 2,
+  y: canvas.height / 2,
+  size: 10,
+  speed: 4,
+  // Direction speed on x and y axis
+  dx: 4,
+  dy: -4
+};
+
+// Create paddle props
+const paddle = {
+  x: canvas.width / 2 - 40, // Entire paddle width is 80, it's in the center so -40
+  y: canvas.height - 20,
+  w: 80,
+  h: 10,
+  speed: 8,
+  dx: 0
+};
+
+// Draw ball on canvas - Canvas API -> Drawing shapes -> Moving the pen -> ctx.arc
+function drawBall() {
+  // Create a new path
+  ctx.beginPath();
+  // Make an outer circle
+  ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+// Draw paddle on canvas
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddle.x, paddle.y, paddle.w, paddle.h);
+  ctx.fillStyle = '#0095dd';
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawScore() {
+  ctx.font = '20px Arial';
+  ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
+
+function draw() {
+  drawPaddle();
+  drawBall();
+  drawScore();
+}
+
+// Draw the game 
+draw();
 
 // Rules and close event handlers
 rulesBtn.addEventListener('click', () => rules.classList.add('show'));
